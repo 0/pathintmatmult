0/pathintmatmult

commit 48de90a6047c93a6cb3a0000417fd7f9b0f79cc5

Generalize to many dimensions

examples/pigs_harmonic_oscillator.py
@@ -65,15 +65,15 @@ if trial_deform is not None:
     alpha = trial_deform * mass * omega / HBAR  # 1/nm^2
 
     def trial_f(q: 'nm') -> '1':
-        return np.exp(-0.5 * alpha * q * q)
+        return np.exp(-0.5 * alpha * q[..., 0] ** 2)
 
     def trial_f_diff(q: 'nm') -> '1/nm^2':
-        return alpha * (alpha * q * q - 1) * trial_f(q)
+        return alpha * (alpha * q[..., 0] ** 2 - 1) * trial_f(q)
 
     kwargs['trial_f'] = trial_f
     kwargs['trial_f_diffs'] = [trial_f_diff]
 
-ho_pigs = PIGSMM(mass, grid_range, grid_len, beta, num_links, harmonic, **kwargs)
+ho_pigs = PIGSMM([mass], [grid_range], [grid_len], beta, num_links, harmonic, **kwargs)
 
 estimated_potential_energy = ho_pigs.expectation_value(harmonic) / KB  # K
 estimated_total_energy = ho_pigs.energy_mixed / KB  # K

examples/pigs_harmonic_oscillator_entangled.py
@@ -10,7 +10,7 @@ from argparse import ArgumentParser
 
 import numpy as np
 
-from pathintmatmult import PIGSMM2
+from pathintmatmult import PIGSMM
 from pathintmatmult.constants import HBAR, KB, ME
 from pathintmatmult.potentials import harmonic_potential
 
@@ -46,7 +46,7 @@ pot_int = harmonic_potential(m=mass, w=omega_int)
 
 
 def total_potential(qs: '[nm]') -> 'kJ/mol':
-    return pot_0(qs[..., 0]) + pot_0(qs[..., 1]) + pot_int(qs[..., 1] - qs[..., 0])
+    return pot_0(qs[..., [0]]) + pot_0(qs[..., [1]]) + pot_int(qs[..., [0]] - qs[..., [1]])
 
 kwargs = {}
 
@@ -69,7 +69,7 @@ if trial_deform is not None:
     kwargs['trial_f'] = trial_f
     kwargs['trial_f_diffs'] = [trial_f_diff_0, trial_f_diff_1]
 
-ho_pigs = PIGSMM2(mass, grid_range, grid_len, beta, num_links, total_potential, **kwargs)
+ho_pigs = PIGSMM([mass, mass], [grid_range, grid_range], [grid_len, grid_len], beta, num_links, total_potential, **kwargs)
 
 estimated_potential_energy = ho_pigs.expectation_value(total_potential) / KB  # K
 estimated_total_energy = ho_pigs.energy_mixed / KB  # K

pathintmatmult/__init__.py
@@ -1,1 +1,1 @@
-from .nmm import PIGSMM, PIGSMM2
+from .nmm import PIGSMM

pathintmatmult/nmm.py
@@ -2,6 +2,8 @@
 Numerical matrix multiplication for path integrals.
 """
 
+from itertools import product
+
 import numpy as np
 
 from .constants import HBAR
@@ -13,15 +15,16 @@ class PIGSMM:
     Path Integral Ground State via Matrix Multiplication
 
     Calculate the approximate ground state wavefunction of a system comprised
-    of a particle in an arbitrary potential on a discretized and truncated
-    grid. The wavefunction is determined via imaginary time propagation from a
-    trial function using numerical matrix multiplication.
+    of one or more particles in an arbitrary potential on a discretized and
+    truncated grid. The wavefunction is determined via imaginary time
+    propagation from a trial function using numerical matrix multiplication.
     """
 
-    def __init__(self, mass: 'g/mol', grid_range: 'nm', grid_len: '1',
-                 beta: 'mol/kJ', num_links: '1', pot_f: 'nm -> kJ/mol', *,
-                 trial_f: 'nm -> 1' = None,
-                 trial_f_diffs: '[nm -> 1/nm^2]' = None):
+    def __init__(self, masses: '[g/mol]', grid_ranges: '[nm]',
+                 grid_lens: '[1]', beta: 'mol/kJ', num_links: '1',
+                 pot_f: '[nm] -> kJ/mol', *,
+                 trial_f: '[nm] -> 1' = None,
+                 trial_f_diffs: '[[nm] -> 1/nm^2]' = None):
         """
         Note:
           The convention used is that beta represents the entire path, so the
@@ -29,36 +32,48 @@ class PIGSMM:
           is beta/2.
 
         Note:
-          When pot_f and trial_f receive an array as input, they need to map
-          over it.
+          When pot_f and trial_f receive an N-dimensional array as input, they
+          need to map over it, returning an (N-1)-dimensional array.
+
+        Note:
+          The "particles" are actually any Cartesian degrees of freedom. One
+          might have the same configuration (masses and grids) for a
+          3-dimensional 1-particle system as for a 1-dimensional 3-particle
+          system. Of course, the coordinate arrays must be interpreted
+          appropriately in each case (whether by the potential function or by
+          the user of the output wavefunction).
 
         Parameters:
-          mass: Mass of the particle.
-          grid_range: Where the grid is truncated. The grid is symmetric about
-                      the origin.
-          grid_len: How many points are on the grid.
+          masses: Masses of the particles.
+          grid_ranges: Where the grids are truncated. Each grid is symmetric
+                       about the origin.
+          grid_lens: How many points are on the grids.
           beta: Propagation length of the entire path.
           num_links: Number of links in the entire path.
-          pot_f: Potential experienced by the particle at some position in
-                 space.
+          pot_f: Potential experienced by the particles in some spatial
+                 configuration.
           trial_f: Approximation to the ground state wavefunction. If none is
                    provided, a uniform trial function is used.
-          trial_f_diffs: Second derivatives of trial_f.
+          trial_f_diffs: Second derivatives of trial_f. One function must be
+                         specified for each particle.
         """
 
-        assert mass >= 0, 'Mass must not be negative.'
-        assert grid_range > 0, 'Grid must have a positive length.'
-        assert grid_len >= 2, 'Grid must have at least two points.'
+        assert len(masses) == len(grid_ranges) == len(grid_lens), \
+            'Numbers of configuration items must match.'
+        assert all(m > 0 for m in masses), 'Masses must be positive.'
+        assert all(gr > 0 for gr in grid_ranges), 'Grids must have positive lengths.'
+        assert all(gl >= 2 for gl in grid_lens), 'Grids must have at least two points.'
         assert beta > 0, 'Beta must be positive.'
         assert num_links > 0, 'Must have at least one link.'
         assert num_links % 2 == 0, 'Number of links must be even.'
 
         if trial_f is not None:
             assert trial_f_diffs is not None, 'Derivatives must be provided.'
+            assert len(trial_f_diffs) == len(masses), 'Number of derivatives must match.'
 
-        self._mass = mass
-        self._grid_range = grid_range
-        self._grid_len = grid_len
+        self._masses = np.array(masses)
+        self._grid_ranges = np.array(grid_ranges)
+        self._grid_lens = np.array(grid_lens)
         self._beta = beta
         self._num_links = num_links
         self._pot_f = pot_f
@@ -69,16 +84,16 @@ class PIGSMM:
         self._cached = {}
 
     @property
-    def mass(self) -> 'g/mol':
-        return self._mass
+    def masses(self) -> '[g/mol]':
+        return self._masses
 
     @property
-    def grid_range(self) -> 'nm':
-        return self._grid_range
+    def grid_ranges(self) -> '[nm]':
+        return self._grid_ranges
 
     @property
-    def grid_len(self) -> '1':
-        return self._grid_len
+    def grid_lens(self) -> '[1]':
+        return self._grid_lens
 
     @property
     def beta(self) -> 'mol/kJ':
@@ -89,8 +104,16 @@ class PIGSMM:
         return self._num_links
 
     @property
-    def pot_f(self) -> 'nm -> kJ/mol':
+    def pot_f(self) -> '[nm] -> kJ/mol':
         return self._pot_f
+
+    @property
+    def trial_f(self) -> '[nm] -> 1':
+        return self._trial_f
+
+    @property
+    def trial_f_diffs(self) -> '[[nm] -> 1/nm^2]':
+        return self._trial_f_diffs
 
     @property
     @cached
@@ -105,37 +128,38 @@ class PIGSMM:
     @cached
     def num_points(self) -> '1':
         """
-        Number of points in the vector.
+        Number of points in the coordinate vector.
         """
 
-        return self.grid_len
+        return np.prod(self.grid_lens)
 
     @property
     @cached
-    def grid(self) -> '[nm]':
+    def grid(self) -> '[[nm]]':
         """
         Vector of the positions corresponding to the grid points.
+
+        This is not a vector in the sense of a 1-dimensional array, because
+        each element is itself a vector of coordinates for each particle.
+        However, it can be thought of as the tensor product of the
+        1-dimensional position vectors.
         """
 
-        return np.linspace(-self.grid_range, self.grid_range, self.grid_len)
+        grids = [np.linspace(-gr, gr, gl) for (gr, gl) in zip(self.grid_ranges, self.grid_lens)]
+        result = np.array(list(product(*grids)))
+
+        assert result.shape == (self.num_points, len(self.masses))
+
+        return result
 
     @property
     @cached
-    def grid_spacing(self) -> 'nm':
-        """
-        Distance between adjacent grid points.
-        """
-
-        return 2 * self.grid_range / (self.grid_len - 1)
-
-    @property
-    @cached
-    def volume_element(self) -> 'nm':
+    def volume_element(self) -> 'nm^N':
         """
         Effective volume taken up by each grid point.
         """
 
-        return self.grid_spacing
+        return np.prod(self.grid_ranges / (self.grid_lens - 1))
 
     @property
     @cached
@@ -162,11 +186,11 @@ class PIGSMM:
         Unnormalized trial function evaluated on the grid.
         """
 
-        if self._trial_f is None:
+        if self.trial_f is None:
             # Default to a uniform trial function.
             return self.uniform_trial_f_grid
 
-        return self._trial_f(self.grid)
+        return self.trial_f(self.grid)
 
     @property
     @cached
@@ -175,7 +199,7 @@ class PIGSMM:
         Unnormalized uniform trial function derivatives evaluated on the grid.
         """
 
-        return np.zeros((1, self.num_points))
+        return np.zeros(self.grid.T.shape)
 
     @property
     @cached
@@ -184,36 +208,41 @@ class PIGSMM:
         Unnormalized trial function derivatives evaluated on the grid.
         """
 
-        if self._trial_f is None:
+        if self.trial_f is None:
             # Default to a uniform trial function.
             return self.uniform_trial_f_diffs_grid
 
-        result = np.empty((len(self._trial_f_diffs), self.num_points))
+        result = np.empty(self.grid.T.shape)
 
-        for i, f in enumerate(self._trial_f_diffs):
+        for i, f in enumerate(self.trial_f_diffs):
             result[i] = f(self.grid)
 
         return result
 
     @property
     @cached
-    def rho_tau(self) -> '[[1/nm]]':
+    def rho_tau(self) -> '[[1/nm^N]]':
         """
         Matrix for the high-temperature propagator.
         """
 
-        prefactor_K = self.mass / (2 * HBAR * HBAR * self.tau)  # 1/nm^2
+        prefactors_K = self.masses / (2 * HBAR * HBAR * self.tau)  # [1/nm^2]
         prefactor_V = self.tau / 2  # mol/kJ
-        prefactor_front = np.sqrt(prefactor_K / np.pi)  # 1/nm
+        prefactor_front = np.sqrt(np.prod(prefactors_K) / np.pi)  # 1/nm^N
 
-        K = np.tile(self.grid, (self.num_points, 1))  # [[nm]]
-        V = np.tile(self.pot_f_grid, (self.num_points, 1))  # [[kJ/mol]]
+        K = np.empty((self.num_points, self.num_points))  # [[nm^2]]
+        V = np.empty_like(K)  # [[kJ/mol]]
 
-        return prefactor_front * np.exp(-prefactor_K * (K - K.T) ** 2 - prefactor_V * (V + V.T))
+        for i, q_i in enumerate(self.grid):
+            for j, q_j in enumerate(self.grid):
+                K[i, j] = np.sum(prefactors_K * (q_i - q_j) ** 2)
+                V[i, j] = self.pot_f(q_i) + self.pot_f(q_j)
+
+        return prefactor_front * np.exp(-K - prefactor_V * V)
 
     @property
     @cached
-    def rho_beta_half(self) -> '[[1/nm]]':
+    def rho_beta_half(self) -> '[[1/nm^N]]':
         """
         Matrix for the half path propagator.
         """
@@ -227,7 +256,7 @@ class PIGSMM:
 
     @property
     @cached
-    def rho_beta(self) -> '[[1/nm]]':
+    def rho_beta(self) -> '[[1/nm^N]]':
         """
         Matrix for the full path propagator.
         """
@@ -272,112 +301,44 @@ class PIGSMM:
         Ground state energy calculated using the mixed estimator.
         """
 
-        ground_wf_full = np.dot(self.rho_beta, self.trial_f_grid)  # [1/nm]
-        trial_f_diffs = np.sum(self.trial_f_diffs_grid / self.mass, axis=0)  # [mol/g nm^2]
+        ground_wf_full = np.dot(self.rho_beta, self.trial_f_grid)  # [1/nm^N]
+        trial_f_diffs = np.sum(self.trial_f_diffs_grid / self.masses[:, np.newaxis], axis=0)  # [mol/g nm^2]
 
-        energy_V = np.sum(ground_wf_full * self.pot_f_grid * self.trial_f_grid)  # kJ/mol nm
-        energy_K = np.dot(ground_wf_full, trial_f_diffs)  # mol/g nm^3
-        normalization = np.dot(ground_wf_full, self.trial_f_grid)  # 1/nm
+        energy_V = np.sum(ground_wf_full * self.pot_f_grid * self.trial_f_grid)  # kJ/mol nm^N
+        energy_K = np.dot(ground_wf_full, trial_f_diffs)  # mol/g nm^(N+2)
+        normalization = np.dot(ground_wf_full, self.trial_f_grid)  # 1/nm^N
 
         return (energy_V - 0.5 * HBAR * HBAR * energy_K) / normalization
 
-    def expectation_value(self, property_f: '[nm] -> [X]') -> 'X':
+    def expectation_value(self, property_f: '[nm] -> X') -> 'X':
         """
         Ground state expectation value of property_f.
+
+        When property_f receives an N-dimensional array as input, it should
+        behave in the same manner as pot_f.
         """
 
         return np.dot(self.density_diagonal, property_f(self.grid))
-
-
-class PIGSMM2(PIGSMM):
-    """
-    PIGSMM for two identical particles.
-    """
-
-    # pot_f(self) -> '[nm] -> kJ/mol'
-
-    @property
-    @cached
-    def num_points(self) -> '1':
-        """
-        Number of points in the vector.
-        """
-
-        return self.grid_len ** 2
-
-    @property
-    @cached
-    def grid(self) -> '[[nm]]':
-        """
-        Vector of the positions corresponding to the grid points.
-
-        Actually a 2xN array, with each row containing coordinates of both
-        particles.
-        """
-
-        single = np.linspace(-self.grid_range, self.grid_range, self.grid_len)  # [nm]
-        double = np.array([[a, b] for a in single for b in single])  # [[nm]]
-
-        return double
-
-    @property
-    @cached
-    def volume_element(self) -> 'nm^2':
-        """
-        Effective volume taken up by each grid point.
-        """
-
-        return self.grid_spacing ** 2
-
-    @property
-    @cached
-    def uniform_trial_f_diffs_grid(self) -> '[1]':
-        """
-        Unnormalized uniform trial function derivatives evaluated on the grid.
-        """
-
-        return np.zeros((2, self.num_points))
-
-    @property
-    @cached
-    def rho_tau(self) -> '[[1/nm^2]]':
-        """
-        Matrix for the high-temperature propagator.
-        """
-
-        prefactor_K = self.mass / (2 * HBAR * HBAR * self.tau)  # 1/nm^2
-        prefactor_V = self.tau / 2  # mol/kJ
-        prefactor_front = prefactor_K / np.pi  # 1/nm^2
-
-        K = np.empty((self.num_points, self.num_points))  # nm^2
-        V = np.empty_like(K)  # kJ/mol
-
-        for i, q_i in enumerate(self.grid):
-            for j, q_j in enumerate(self.grid):
-                K[i, j] = np.sum((q_i - q_j) ** 2)
-                V[i, j] = self.pot_f(q_i) + self.pot_f(q_j)
-
-        return prefactor_front * np.exp(-prefactor_K * K - prefactor_V * V)
-
-    # rho_beta_half(self) -> '[[1/nm^2]]'
-
-    # rho_beta(self) -> '[[1/nm^2]]'
-
-    # expectation_value(self, property_f: '[[nm]] -> [X]') -> 'X'
 
     @property
     @cached
     def density_reduced(self) -> '[[1]]':
         """
-        Density matrix for one particle, with the other traced out.
+        Density matrix for the first particle, with the other traced out.
+
+        Only implemented for two-particle systems.
         """
 
-        density_new = np.zeros((self.grid_len, self.grid_len))
+        assert len(self.masses) == 2
 
-        for i in range(self.grid_len):
-            for j in range(self.grid_len):
-                for t in range(self.grid_len):
-                    density_new[i, j] += self.density[self.grid_len * i + t, self.grid_len * j + t]
+        new_len = self.grid_lens[0]
+        other_len = self.grid_lens[1]
+        density_new = np.zeros((new_len, new_len))
+
+        for i in range(new_len):
+            for j in range(new_len):
+                for t in range(other_len):
+                    density_new[i, j] += self.density[other_len * i + t, other_len * j + t]
 
         return density_new
 

pathintmatmult/potentials.py
@@ -2,6 +2,8 @@
 Example potential functions.
 """
 
+import numpy as np
+
 
 def free_particle_potential() -> 'nm -> kJ/mol':
     """
@@ -9,8 +11,8 @@ def free_particle_potential() -> 'nm -> kJ/mol':
     """
 
     def free_particle(q: 'nm') -> 'kJ/mol':
-        # Make sure the output is the same shape as the input.
-        return 0 * q
+        # Remove the inner-most dimension.
+        return np.zeros(q.shape[:-1])
 
     return free_particle
 
@@ -36,6 +38,6 @@ def harmonic_potential(k: 'kJ/mol nm^2' = None, m: 'g/mol' = None, w: '1/ps' = N
         assert False, 'Must provide either k or (m and w).'
 
     def harmonic(q: 'nm') -> 'kJ/mol':
-        return force_constant * q * q / 2
+        return force_constant * q[..., 0] * q[..., 0] / 2
 
     return harmonic

commit 18aa0d2f328bbf36098a7bb85f4929ccf64da85f

Fix --wf-out
It was broken in 48de90a when the shape of the grid array changed.

examples/pigs_harmonic_oscillator.py
@@ -86,7 +86,7 @@ print('E_mixed = {} K'.format(estimated_total_energy))
 
 # Output wavefunction.
 if wf_out:
-    np.savetxt(wf_out, np.dstack((ho_pigs.grid, ho_pigs.ground_wf))[0])
+    np.savetxt(wf_out, np.hstack((ho_pigs.grid, ho_pigs.ground_wf[:, np.newaxis])))
 
 # Output plot.
 if density_out: